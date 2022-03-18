c7a6041bf8b2a97f1bef548e5f05ac9335845e5c
José H. Garcia <josehugo.garcia@protonmail.com>
2022-03-18 11:47:12 +0100

    added an example of read from xyz

---
+++ examples/example7_assymetric_structure/PdSe2.relax.py
@@ -0,0 +1,51 @@
+# Package directory
+import os
+from re import M
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('.'))
+
+from math import sqrt
+import qe_suite as qes
+from qe_suite.builder import Structure, System, PWInput
+from qe_suite.builder.calculation import Relaxation, Bands
+
+#First we define the structure of the system
+hexagonal= Structure( cell = [(2.467, 0.000, 0.0), (-1.234, 2.137, 0.0), (0.000, 0.000, 15.0)],
+                      fractional_positions = [ (0.6667, 0.3333, 0.5), (0.3333, 0.6667, 0.5) ],
+                      atomic_symbols = ["C", "C"], 
+                      symmetrize=True);
+hexagonal.set_as_2D();
+
+#That structure is used to define the system to be study
+graphene = System();
+graphene.set_atomic_species( ["C"], use_SSSP= "../SSSP/SSSP_1.1.2_PBEsol_precision.json" );
+graphene.set_structure(hexagonal, use_symmetries=True);
+
+#Then we create a self consistent calculation to determine the density matrix
+relax = Relaxation().set_pseudopot_dir("../SSSP").set_cell_do_free('ibrav');
+relax.set_k_points("automatic", hexagonal.get_kpoints(type="automatic"))
+
+
+pw_input = PWInput(calculation = relax, system=graphene  );
+inpfile=pw_input.write("qe_suite.relax.inp")
+qes.run_pw(inpfile=pw_input.write("qe_suite.relax.inp"),shell=False );
+
+#Finally we analyze the system by performing a band structure calculation
+bands = Bands(scf=relax).set_band_path( hexagonal.symmetrized_band_path() );
+pw_input = PWInput(calculation = bands, system=graphene.set_numbands(5) );
+inpfile=pw_input.write("qe_suite.bands.inp")
+qes.run_pw(inpfile=pw_input.write("qe_suite.bands.inp") );
+
+
+import qe_suite.parse as parse
+import matplotlib.pyplot as plt
+
+bs = parse.BandStructure(xml="qe_suite/qe_suite.xml");
+hsp =hexagonal.get_symmetrized_high_symmetry_points();
+
+for band in bs.bands(shift_Efermi=True):
+    plt.plot(*band);
+plt.xticks( *bs.xticks_high_symmetry_points( hsp= hsp ) );
+plt.ylabel( bs.get_ylabel() );
+plt.savefig('band_structure.pdf');  
